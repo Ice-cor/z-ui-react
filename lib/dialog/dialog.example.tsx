ffb2008033626e1e707705ea3a9676340f6e0c0c
zhongwenqin <zhongwenqin@weshare.com.cn>
2020-03-26 17:20:36 +0800

    dialog组件添加可控按钮

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,22 +1,42 @@
 import React, {useState} from 'react';
-import Dialog from './dialog';
+import Dialog, {confirm} from './dialog';
 
 const IconExample: React.FC = () => {
-    const [visible, setVisible] = useState(false);
-    return (
-        <div>
-            <h2>例子1：</h2>
-            <button onClick={() => {setVisible(!visible);}}>打开dialog</button>
-            <Dialog
-                visible={visible}
-                onOk={() => {setVisible(false);}}
-                onCancel={() => {setVisible(false);}}
-            >
-                <h3>看到我了吗？</h3>
-                <h4>呵呵</h4>
-            </Dialog>
-        </div>
-    );
-};
+        const [visible, setVisible] = useState(false);
+        const [visible2, setVisible2] = useState(false);
+        return (
+            <div>
+                <div>
+                    <h2>例子1：</h2>
+                    <button onClick={() => {setVisible(!visible);}}>打开dialog</button>
+                    <button onClick={() => {setVisible2(true);}}>打开自定义footer</button>
+                    <Dialog
+                        visible={visible}
+                        onOk={() => {setVisible(false);}}
+                        onCancel={() => {setVisible(false);}}
+                        okText="nima"
+                    >
+                        <h3>看到我了吗？</h3>
+                        <h4>呵呵</h4>
+                    </Dialog>
+                    <Dialog
+                        visible={visible2}
+                        onOk={() => {setVisible2(false);}}
+                        onCancel={() => {setVisible2(false);}}
+                        okText="hehe"
+                        footer={[<button onClick={()=> {setVisible2(false)}}>hehe</button>, <button>你好呀</button>]}
+                    >
+                        <h3>看到我了吗？</h3>
+                        <h4>呵呵</h4>
+                    </Dialog>
+                </div>
+                <div>
+                    <h2>例子2：</h2>
+                    <button onClick={() => {confirm('123');}}>confirm</button>
+                </div>
+            </div>
+        );
+    }
+;
 
 export default IconExample;
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,4 +1,5 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, ReactElement} from 'react';
+import ReactDOM from 'react-dom';
 
 import {scopedClassMaker} from '../_util/classes';
 import Icon from '../icon/icon';
@@ -8,35 +9,60 @@ interface Props {
     visible: boolean;
     onOk?: () => void;
     onCancel?: () => void;
+    footer?: Array<ReactElement>;
+    okText?: string;
+    cancelText?: string;
 }
 
 const scopedClass = scopedClassMaker('zui-dialog');
 const sc = scopedClass;
 
 const Dialog: React.FC<Props> = (props) => {
-    return (
-        props.visible ?
-            <Fragment>
-                <div className={sc('mask')}>dialogWrap</div>
-                <div className={sc()}>
-                    <div className={sc('content')}>
-                        <div className={sc('header')}>
-                            <h4 className={sc('title')}>Title</h4>
-                            <Icon name="close" className={sc('close')} onClick={props.onCancel}/>
-                        </div>
-                        <div className={sc('body')}>
-                            {props.children}
-                        </div>
-                        <div className={sc('footer')}>
-                            <button onClick={props.onOk}>Ok</button>
-                            <button onClick={props.onCancel}>Cancel</button>
-                        </div>
+    const modal = props.visible ?
+        <Fragment>
+            <div className={sc('mask')}>dialogWrap</div>
+            <div className={sc()}>
+                <div className={sc('content')}>
+                    <Icon name="close" className={sc('close')} onClick={props.onCancel} />
+                    <div className={sc('header')}>
+                        <h4 className={sc('title')}>Title</h4>
+                    </div>
+                    <div className={sc('body')}>
+                        {props.children}
+                    </div>
+                    <div className={sc('footer')}>
+                        {
+                            props.footer ? props.footer.map((item, index) =>
+                                    React.cloneElement(item, {key: item.key || index})
+                                ) :
+                                <Fragment>
+                                    <button onClick={props.onOk}>
+                                        {props.okText || 'Ok'}
+                                    </button>
+                                    <button onClick={props.onCancel}>
+                                        {props.cancelText || 'Cancel'}
+                                    </button>
+                                </Fragment>
+                        }
                     </div>
                 </div>
-            </Fragment>
-            :
-            null
-    );
+            </div>
+        </Fragment>
+        :
+        null;
+    return ReactDOM.createPortal(modal, document.body);
 };
 
+const confirm = (content: string) => {
+    const hide = () => {
+        ReactDOM.unmountComponentAtNode(div);
+        document.body.removeChild(div);
+    };
+    const component = <Dialog visible={true} onCancel={hide}>{content}</Dialog>;
+    const div = document.createElement('div');
+    ReactDOM.render(component, div);
+    document.body.appendChild(div);
+    return hide;
+};
+export {confirm};
 export default Dialog;
