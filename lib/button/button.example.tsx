5e6b63c40898eeb099d216fccdea5f52377025d5
zhongwenqin <zhongwenqin@weshare.com.cn>
2020-04-03 15:12:54 +0800

    完成button组件大致功能

--- lib/button/button.example.tsx
+++ lib/button/button.example.tsx
@@ -5,7 +5,7 @@ import Button from './button';
 const ButtonExample: React.FC = () => {
     return (
         <div>
-            <Button>点击</Button>
+            <Button onClick={()=>{console.log('点击了')}}>点击</Button>
         </div>
     )
 }
--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import {scopedClassMaker} from '../_util/classes';
 import './style/button.scss'
 interface Props {
-
+    onClick?: React.MouseEventHandler;
 }
 
-const scopedClass = scopedClassMaker('zui-button');
+const scopedClass = scopedClassMaker('zui-btn');
 const sc = scopedClass;
 
 const Button: React.FC<Props> = (props) => {
     return (
-        <button className={sc()}>
+        <button className={`zui-btn ${sc('default')}`} onClick={props.onClick}>
             {props.children}
         </button>
     )
