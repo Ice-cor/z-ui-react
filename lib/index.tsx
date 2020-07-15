de180258454e4219d335edeaf0124c066f23eb83
钟先生 <zhong0102@qq.com>
2020-07-15 22:23:49 +0800

    修改md文件

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,6 @@
 export {default as Icon} from './icon/icon';
+export {default as Button} from './button/button'
+export {default as Layout} from './layout/layout'
+export {default as Dialog} from './dialog/dialog'
+export {default as Input} from './input/input'
+export {default as Form} from './form/form'
